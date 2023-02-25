fe08ea41de2f5928df654aeb1702c7455af86415
00lartks <eduardani84@gmail.com>
2023-02-25 01:29:38 -0700

    Primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
+const parrafito = document.querySelector(".parrafito");
+const input = document.querySelector("input");
+const pid = document.querySelector("#pid");
+
+console.log({ h1, p, parrafito, input, pid });
