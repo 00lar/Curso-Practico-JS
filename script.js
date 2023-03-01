9b7f91098d0c26844e3d0e383cd7be03e9ed7b95
00lartks <eduardani84@gmail.com>
2023-03-01 00:05:19 -0700

    practica de suma de strings

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.getElementsByClassName("parrafito");
-const input = document.querySelector("input");
-const pid = document.getElementById("pid");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const presult = document.querySelector("#result");
 
-console.log({ h1, p, parrafito, input, pid });
-
-h1.innerHTML = "Patito <br> feo";
-h1.innerText = "patito <br> feo";
-
-h1.classList.add("rojo")
-
-const img = document.createElement("img")
-img.setAttribute("src", "https://phantom-marca.unidadeditorial.es/0461a93910cddc9ac00c7afc145fac1f/crop/92x0/1219x751/resize/1320/f/jpg/assets/multimedia/imagenes/2023/01/11/16734568244270.jpg");
-console.log(img)
-
-pid.innerHTML = "";
-
-pid.appendChild(img);
+function btnOnClick() {
+  const sumaInputs = input1.value + input2.value;
+  presult.innerText = "Resultado " + sumaInputs;
+}
